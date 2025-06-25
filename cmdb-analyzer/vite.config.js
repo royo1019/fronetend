1f1148b98f9bc8eb65c7b637896106a925cc3226
Shivani <bshivani1505@gmail.com>
2025-06-25 10:39:48 +0530

    frontend deploy tweaks

--- cmdb-analyzer/vite.config.js
+++ cmdb-analyzer/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/fronetend/' : '/',
-  build: {
+  base: '/',
+  build: {  
     outDir: 'dist',
     assetsDir: 'assets',
   },
